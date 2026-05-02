896241ee1b4647750ef6df87e93e7558e26690d5
Mamun123123123 <almalm90459@gmail.com>
2026-05-02 19:57:47 +0600

    object in typescript

--- src/nonprimitive.ts
+++ src/nonprimitive.ts
@@ -3,6 +3,32 @@
 let arr = [1,2,3,4]
 let str:(string | number)[]= [1,"mamun"]
 
-console.log(str);
+// console.log(str);
 
 let all:[string,boolean,boolean] = ["hello",false,true]
+
+//reference type : object
+
+// object type
+const user: {
+  readonly organization: string;
+  firstName: string;
+  LastName: string;
+  isActive: boolean;
+  description: {
+    address: string;
+    location: string;
+  };
+} = {
+  organization: "Hello",
+  firstName: "Abdullah",
+  LastName: "Mamun",
+  isActive: true,
+  description: {
+    address: "Dhaka",
+    location: "None",
+  },
+};
+
+const x = user.description.address;
+console.log(x);
