ce45559f0aceee2bf1919666c09e3e77d15e9672
Mamun123123123 <almalm90459@gmail.com>
2026-05-04 21:05:25 +0600

    basic types

---
+++ src/test1.ts
@@ -0,0 +1,11 @@
+let a = 12; //number
+let x;//any
+let p :any;
+let b:number;
+let z :string;
+let u: boolean;
+let y :[];
+let arr:[number,string]=[4,"mamun"] //tuple
+let arr2:[string,number,boolean]=["heelo",2,true]//tuple
+
+let variable :unknown;
