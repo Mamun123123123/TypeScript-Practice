b5b54971acfb930f93122ed0fc781c6821e0509e
Mamun123123123 <almalm90459@gmail.com>
2026-05-04 22:34:50 +0600

    union

--- src/test1.ts
+++ src/test1.ts
@@ -1,11 +1,22 @@
-let a = 12; //number
-let x;//any
-let p :any;
-let b:number;
-let z :string;
-let u: boolean;
-let y :[];
-let arr:[number,string]=[4,"mamun"] //tuple
-let arr2:[string,number,boolean]=["heelo",2,true]//tuple
+// let a = 12; //number
+// let x;//any
+// let p :any;
+// let b:number;
+// let z :string;
+// let u: boolean;
+// let y :[];
+// let arr:[number,string]=[4,"mamun"] //tuple
+// let arr2:[string,number,boolean]=["heelo",2,true]//tuple
 
-let variable :unknown;
+// let variable :unknown;
+
+//union and Intersection
+
+let variable: string |  null ;
+
+variable = "hello"
+variable = null
+
+let str : string | null;
+
+
