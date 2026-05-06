def4fe97ad60461aba6c7fea8970fec8ee56874c
Mamun123123123 <almalm90459@gmail.com>
2026-05-06 14:04:59 +0600

    type of Guard in typescript

---
+++ src/typeGuard.ts
@@ -0,0 +1,11 @@
+const add = (num1 : number | string,num2:number|string) =>{
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        return num1+num2
+    }else{
+
+       return num1.toString() + num2.toString()
+    }
+}
+console.log(add(2,"3"));
+
+// in guard
