8a46bad9192578bc7c5dc1fd08c23315630f7c47
Mamun123123123 <almalm90459@gmail.com>
2026-05-08 19:26:31 +0600

    sub in type guards

--- src/typeGuard.ts
+++ src/typeGuard.ts
@@ -7,5 +7,14 @@ const add = (num1 : number | string,num2:number|string) =>{
     }
 }
 console.log(add(2,"3"));
+const sub = (num1 : number | string,num2:number|string) =>{
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        return num1-num2
+    }else{
+
+       return num1.toString() + num2.toString()
+    }
+}
+console.log(sub(2,"3"));
 
 // in guard
